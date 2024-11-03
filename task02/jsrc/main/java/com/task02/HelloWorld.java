5088da4fd9df0a12b3a84c861c41aca2ce01dacb
Chidvila <somasani_chidvila@epam.com>
2024-11-03 17:12:34 +0530

    task2

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -28,22 +28,29 @@ import java.util.Map;
 		invokeMode = InvokeMode.BUFFERED // This can be set to BUFFERED or STREAMING based on your requirements
 
 )
+
 public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-		if (request.getPath().equals("/hello") && request.getHttpMethod().equalsIgnoreCase("GET")) {
-			response.setStatusCode(200);
-			response.setBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
-		} else {
-			response.setStatusCode(400);
-			response.setBody("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: "
-					+ request.getPath()
-					+ ". HTTP method: "
-					+ request.getHttpMethod()
-					+ "\"}");
+		@Override
+		public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+			APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+			Map<String, String> headers = new HashMap<>();
+			headers.put("Content-Type", "application/json");
+
+			if ("/hello".equals(request.getPath()) && "GET".equalsIgnoreCase(request.getHttpMethod())) {
+				response.setStatusCode(200);
+				response.setHeaders(headers);
+				response.setBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+			} else {
+				response.setStatusCode(400);
+				response.setHeaders(headers);
+				response.setBody("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: "
+						+ request.getPath()
+						+ ". HTTP method: "
+						+ request.getHttpMethod()
+						+ "\"}");
+			}
+
+			return response;
 		}
-		response.setHeaders(Collections.singletonMap("Content-Type", "application/json"));
-		return response;
 	}
-}
